2c341b9642bfcb7d19f0eec38ba287144623a1f8
Chris Ziegler <chrisrziegler@gmail.com>
2021-10-10 00:58:05 -0700

    Integrate MongoDB, dotenv

--- index.js
+++ index.js
@@ -1,83 +1,50 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content:
-      'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
-  },
-]
-
-// The Root Route
-// Replaced by app.use(express.static('build'))
-// app.get('/', (request, response) => {
-//   response.send('<h1>Hello World!</h1>')
-// })
-
-// The Index Route
+// The Get All
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 // The Show Route
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  //note variable set to 'undefined if no matching note found
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    //override the default http error code "reason-phrase"
-    response.statusMessage = 'Requested note resource does not exist'
-    response
-      .status(404)
-      .json({ error: 'Requested note resource does not exist' })
-  }
+  })
 })
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-  return maxId + 1
-}
+// const generateId = () => {
+//   const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+//   return maxId + 1
+// }
 
 // The Post Route
 app.post('/api/notes', (request, response) => {
   const { body } = request
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing',
     })
   }
 
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
-    date: new Date().toISOString(),
     important: body.important || false,
-  }
+    date: new Date(),
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 // The Destroy Route
@@ -93,7 +60,8 @@ const unkownEndpoint = (request, response) => {
 
 app.use(unkownEndpoint)
 
-const PORT = process.env.PORT || 3001
+// this variable is defined dynamically by the dev or production environments
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
 })
