2b4665d075264c7a55b84622f37a77f3480b1b7f
Chris Ziegler <chrisrziegler@gmail.com>
2021-10-10 23:13:53 -0700

    Add request logger, delete and update routes, proper error handling middleware

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 require('dotenv').config()
 const express = require('express')
+// const path = require('path')
+// const fs = require('fs')
 const app = express()
 const cors = require('cors')
+const morgan = require('morgan')
 const Note = require('./models/note')
 
+morgan.token('info', req => JSON.stringify(req.info))
+
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] :response-time ms :info',
+    // { stream: appLogStream },
+  ),
+)
 
 // The Get All
 app.get('/api/notes', (request, response) => {
@@ -16,10 +27,16 @@ app.get('/api/notes', (request, response) => {
 })
 
 // The Show Route
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 // const generateId = () => {
 //   const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
@@ -29,6 +46,7 @@ app.get('/api/notes/:id', (request, response) => {
 // The Post Route
 app.post('/api/notes', (request, response) => {
   const { body } = request
+  request.info = body
 
   if (body.content === undefined) {
     return response.status(400).json({
@@ -47,18 +65,47 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-// The Destroy Route
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+// Update Route
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
-  response.status(204).end()
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
+
+// Delete Route
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
 const unkownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unkownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 // this variable is defined dynamically by the dev or production environments
 const PORT = process.env.PORT
